56b7d0c55ad5261fe41fb80f6f0ccf6fd6287520
Даниил Шерстенников <70951451+SherstennikovDaniil@users.noreply.github.com>
2022-10-22 03:30:41 +0300

    Done get_categories and get_category_products func

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from json import dump
+from typing import List
+import logging
 
 from requests import get
+from requests.exceptions import JSONDecodeError
+
+logging.basicConfig(level=logging.INFO)
 
 
 def write_to_json_file(data):
@@ -8,13 +13,11 @@ def write_to_json_file(data):
         dump(data, f, indent=4, ensure_ascii=False)
 
 
-def get_category_info(category_id: int, offset: int = 0) -> dict | None:
-    url = "https://5d.5ka.ru/api/cita/v4/categories/preview/"
-
+def get_categories(store: str = "324G"):
     headers = {
         "Host": "5d.5ka.ru",
         "Accept": "application/json",
-        "X-USER-STORE": "324G",
+        "X-USER-STORE": store,
         "Accept-Language": "en-RU;q=1.0, ru-RU;q=0.9",
         "X-PACKAGE-NAME": "com.antimarket.pyaterochkadelivery",
         "User-Agent": "delivery_release/4.3.6 (com.antimarket.pyaterochkadelivery; build:1; iOS 16.0.0) Alamofire/5.6.1",
@@ -23,29 +26,59 @@ def get_category_info(category_id: int, offset: int = 0) -> dict | None:
         "X-PLATFORM": "ios",
         "X-CAN-RECEIVE-PUSH": "false",
     }
+
+    resp = get(
+        "https://5d.5ka.ru/api/cita/v5/categories/",
+        headers=headers,
+    )
+    if resp.ok:
+        try:
+            return resp.json()
+        except JSONDecodeError:
+            return []
+
+
+def get_category_products(
+    category_id: int, offset: int = 0, store: str = "324G"
+) -> List[dict]:
+    headers = {
+        "Host": "5d.5ka.ru",
+        "Accept": "application/json",
+        "X-USER-STORE": store,
+        "Accept-Language": "en-RU;q=1.0, ru-RU;q=0.9",
+        "X-PACKAGE-NAME": "com.antimarket.pyaterochkadelivery",
+        "User-Agent": "delivery_release/4.3.6 (com.antimarket.pyaterochkadelivery; build:1; iOS 16.0.0) Alamofire/5.6.1",
+        "X-DEVICE-ID": "7406AAA7-D1B6-4326-8DD2-BFC75CE5CD2D",
+        "X-APP-VERSION": "4.3.6",
+        "X-PLATFORM": "ios",
+        "X-CAN-RECEIVE-PUSH": "false",
+    }
+
     params = {
         "category": f"{category_id}",
-        "is_new": "0",
-        "is_promo": "0",
+        "limit": "100",
         "offset": f"{offset}",
-        "rating_order": "average_rating_desc",
     }
 
     resp = get(
-        url,
+        "https://5d.5ka.ru/api/cita/v1/products/",
         params=params,
-        # cookies=cookies,
         headers=headers,
     )
-    print(resp.text)
-    print(resp.status_code)
-
     if resp.ok:
-        return resp.json()
-    else:
-        return None
+        try:
+            return resp.json()
+        except JSONDecodeError:
+            return []
+
+
+def main():
+    data = get_categories()
+    for category in data:
+        products = get_category_products(category["id"])
+        write_to_json_file(products)
+        input()
 
 
 if __name__ == "__main__":
-    data = get_category_info(716)
-    write_to_json_file(data)
+    main()
